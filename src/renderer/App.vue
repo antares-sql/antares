4b6ed99785b1584e77972a9f5f66410eab6a5265
Fabio <fabio286@gmail.com>
2020-04-30 17:48:53 +0200

    Structuring project

---
+++ src/renderer/App.vue
@@ -0,0 +1,32 @@
+<template>
+   <div id="app">
+      <img alt="Vue logo" src="logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+   </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+   name: 'App',
+   components: {
+      HelloWorld
+   }
+};
+</script>
+
+<style>
+body{
+  background: #FFFFFF;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
