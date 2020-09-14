264829bec0093b2cd41f3c571d4615d7b3f5d9b7
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-09-14 12:31:42 +0200

    Update it-IT.js

--- src/renderer/i18n/it-IT.js
+++ src/renderer/i18n/it-IT.js
@@ -66,7 +66,7 @@ module.exports = {
       editCell: 'Modifica cella',
       deleteRows: 'Elimina riga | Elimina {count} righe',
       confirmToDeleteRows: 'Confermi di voler cancellare una riga? | Confermi di voler cancellare {count} righe?',
-      notificationsTimeout: 'Notifica scaduta',
+      notificationsTimeout: 'Timeout Notifica',
       uploadFile: 'Carica file',
       addNewRow: 'Aggiungi nuova riga',
       numberOfInserts: 'Numero di insert',
