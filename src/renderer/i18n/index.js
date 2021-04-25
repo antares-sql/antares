8720bcdad6a7a246c667e75e7abbf68db1053e3e
Daniel Aguiar <daniel.aguiar@ricardoeletro.com.br>
2021-04-25 03:53:21 -0300

    New Translation: Portugues (Brasil)

--- src/renderer/i18n/index.js
+++ src/renderer/i18n/index.js
@@ -9,7 +9,8 @@ const i18n = new VueI18n({
       'it-IT': require('./it-IT'),
       'ar-SA': require('./ar-SA'),
       'es-ES': require('./es-ES'),
-      'fr-FR': require('./fr-FR')
+      'fr-FR': require('./fr-FR'),
+      'pt-BR': require('./pt-BR')      
    }
 });
 export default i18n;
