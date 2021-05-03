9dfe7cca2234ce6de512bb4c21205c2217e7f765
Fabio Di Stasio <fabio286@gmail.com>
2021-05-03 22:55:39 +0200

    fix(UI): data type figure twice on type select

--- src/renderer/components/WorkspacePropsTableRow.vue
+++ src/renderer/components/WorkspacePropsTableRow.vue
@@ -381,10 +381,12 @@ export default {
       isInDataTypes () {
          let typeNames = [];
          for (const group of this.dataTypes) {
-            typeNames = group.types.reduce((acc, curr) => {
+            const groupTypeNames = group.types.reduce((acc, curr) => {
                acc.push(curr.name);
                return acc;
             }, []);
+
+            typeNames = [...groupTypeNames, ...typeNames];
          }
          return typeNames.includes(this.row.type);
       }
