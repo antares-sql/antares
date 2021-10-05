76e5849c782c600c3477c942d173bbf4e8e66499
isamu <isamu@rnd.co.jp>
2021-10-05 17:22:07 +0900

    Add new language: Japanese

--- src/renderer/i18n/supported-locales.js
+++ src/renderer/i18n/supported-locales.js
@@ -1,10 +1,10 @@
 export default {
-    'en-US': 'English',
-    'it-IT': 'Italiano',
-    'ar-SA': 'العربية',
-    'es-ES': 'Español',
-    'fr-FR': 'Français',
-    'pt-BR': 'Português (Brasil)',
-    'de-DE': 'Deutsch (Deutschland)',
-    'vi-VN': 'Tiếng Việt'
+   'en-US': 'English',
+   'it-IT': 'Italiano',
+   'ar-SA': 'العربية',
+   'es-ES': 'Español',
+   'fr-FR': 'Français',
+   'pt-BR': 'Português (Brasil)',
+   'de-DE': 'Deutsch (Deutschland)',
+   'ja-JP': '日本語'
 };
