b2348639abe629e4abd60c213b2b799145c9e7bc
Fabio Di Stasio <fabio286@gmail.com>
2022-01-29 09:51:33 +0100

    test: temporary fix on Windows tests

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,10 +1,19 @@
 const { _electron: electron } = require('playwright');
 const { strict: assert } = require('assert');
 
+const isWindows = process.platform === 'win32';
+async function wait (ms) {
+   return new Promise(resolve => {
+      setTimeout(resolve, ms);
+   });
+}
+
 (async () => {
    console.log('Starting tests');
    // Launch Electron app.
    const electronApp = await electron.launch({ args: ['dist/main.js'] });
+
+   if (isWindows) await wait(5000);
 
    /**
      * App main window state
@@ -12,7 +21,6 @@ const { strict: assert } = require('assert');
      */
    const windowState = await electronApp.evaluate(({ BrowserWindow }) => {
       const mainWindow = BrowserWindow.getAllWindows()[0];
-
       const getState = () => ({
          isVisible: mainWindow.isVisible(),
          isDevToolsOpened: mainWindow.webContents.isDevToolsOpened()
