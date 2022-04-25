88eb9f7ab80c407ec6517206c27cce715c41eb24
Giulio Ganci <giulioganci@gmail.com>
2022-04-25 22:51:24 +0200

    fix: can not use vue teleport with props as root element

--- src/renderer/components/BaseConfirmModal.vue
+++ src/renderer/components/BaseConfirmModal.vue
@@ -1,47 +1,49 @@
 <template>
-   <Teleport to="#window-content">
-      <div class="modal active" :class="modalSizeClass">
-         <a class="modal-overlay" @click="hideModal" />
-         <div class="modal-container">
-            <div v-if="hasHeader" class="modal-header pl-2">
-               <div class="modal-title h6">
-                  <slot name="header" />
+   <div class="dummy-wrapper">
+      <Teleport to="#window-content">
+         <div class="modal active" :class="modalSizeClass">
+            <a class="modal-overlay" @click="hideModal" />
+            <div class="modal-container">
+               <div v-if="hasHeader" class="modal-header pl-2">
+                  <div class="modal-title h6">
+                     <slot name="header" />
+                  </div>
+                  <a class="btn btn-clear float-right" @click="hideModal" />
                </div>
-               <a class="btn btn-clear float-right" @click="hideModal" />
-            </div>
-            <div v-if="hasDefault" class="modal-header">
-               <div class="modal-title h6">
-                  <slot />
+               <div v-if="hasDefault" class="modal-header">
+                  <div class="modal-title h6">
+                     <slot />
+                  </div>
+                  <a class="btn btn-clear float-right" @click="hideModal" />
                </div>
-               <a class="btn btn-clear float-right" @click="hideModal" />
-            </div>
-            <div v-if="hasBody" class="modal-body">
-               <a
-                  v-if="!hasHeader && !hasDefault"
-                  class="btn btn-clear float-right"
-                  @click="hideModal"
-               />
-               <div class="content">
-                  <slot name="body" />
+               <div v-if="hasBody" class="modal-body">
+                  <a
+                     v-if="!hasHeader && !hasDefault"
+                     class="btn btn-clear float-right"
+                     @click="hideModal"
+                  />
+                  <div class="content">
+                     <slot name="body" />
+                  </div>
                </div>
-            </div>
-            <div v-if="!hideFooter" class="modal-footer pt-0">
-               <button
-                  class="btn btn-primary mr-2"
-                  @click.stop="confirmModal"
-               >
-                  {{ confirmText || $t('word.confirm') }}
-               </button>
-               <button
-                  class="btn btn-link"
-                  @click="hideModal"
-               >
-                  {{ cancelText || $t('word.cancel') }}
-               </button>
+               <div v-if="!hideFooter" class="modal-footer pt-0">
+                  <button
+                     class="btn btn-primary mr-2"
+                     @click.stop="confirmModal"
+                  >
+                     {{ confirmText || $t('word.confirm') }}
+                  </button>
+                  <button
+                     class="btn btn-link"
+                     @click="hideModal"
+                  >
+                     {{ cancelText || $t('word.cancel') }}
+                  </button>
+               </div>
             </div>
          </div>
-      </div>
-   </Teleport>
+      </Teleport>
+   </div>
 </template>
 
 <script>
