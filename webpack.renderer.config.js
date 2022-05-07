da6481f458f731a504bd5e9d5a5a5a99d2415f3e
Fabio Di Stasio <fabio286@gmail.com>
2022-05-07 22:50:11 +0200

    build: fix compilation issues

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -14,7 +14,7 @@ const parsedContributors = contributors.reduce((acc, c) => {
 }, []).join(',');
 
 const isDevMode = process.env.NODE_ENV !== 'production';
-const whiteListedModules = ['.bin', 'vue', '@vue', 'pinia', 'vue-i18n'];
+const whiteListedModules = ['.bin'];
 const externals = {};
 
 fs.readdirSync('node_modules')
