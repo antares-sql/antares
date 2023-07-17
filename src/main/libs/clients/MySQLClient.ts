7c820b18276e4ed5e381d91110acd032862a5be9
Fabio Di Stasio <fabio286@gmail.com>
2023-07-17 18:00:40 +0200

    fix(MySQL): unable to set more than one value in SET fields

--- src/main/libs/clients/MySQLClient.ts
+++ src/main/libs/clients/MySQLClient.ts
@@ -582,7 +582,7 @@ export class MySQLClient extends AntaresCore {
       return rows.map((field) => {
          const numLengthMatch = field.COLUMN_TYPE.match(/int\(([^)]+)\)/);
          const numLength = numLengthMatch ? +numLengthMatch.pop() : field.NUMERIC_PRECISION || null;
-         const enumValues = /(enum|set)/.test(field.COLUMN_TYPE)
+         const enumValues = /(enum)/.test(field.COLUMN_TYPE)
             ? field.COLUMN_TYPE.match(/\(([^)]+)\)/)[0].slice(1, -1)
             : null;
 
