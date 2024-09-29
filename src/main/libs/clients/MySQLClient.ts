7969294a93a51861c57d4396c7a0d89ecc7e8a84
Fabio Di Stasio <fabio286@gmail.com>
2024-09-29 13:50:22 +0200

    fix(MySQL): incorrect representation of the DATE if the year is prior to 1900, fixes #860

--- src/main/libs/clients/MySQLClient.ts
+++ src/main/libs/clients/MySQLClient.ts
@@ -232,12 +232,13 @@ export class MySQLClient extends BaseClient {
       const dbConfig = await this.getDbConfig();
       const connection = await mysql.createConnection({
          ...dbConfig,
-         typeCast: (field, next) => {
-            if (field.type === 'DATETIME')
-               return field.string();
-            else
-               return next();
-         }
+         dateStrings: true
+         // typeCast: (field, next) => {
+         //    if (field.type === 'DATETIME')
+         //       return field.string();
+         //    else
+         //       return next();
+         // }
       });
 
       return connection;
@@ -249,12 +250,13 @@ export class MySQLClient extends BaseClient {
          ...dbConfig,
          connectionLimit: this._poolSize,
          enableKeepAlive: true,
-         typeCast: (field, next) => {
-            if (field.type === 'DATETIME')
-               return field.string();
-            else
-               return next();
-         }
+         dateStrings: true
+         // typeCast: (field, next) => {
+         //    if (field.type === 'DATETIME')
+         //       return field.string();
+         //    else
+         //       return next();
+         // }
       });
 
       this._keepaliveTimer = setInterval(async () => {
