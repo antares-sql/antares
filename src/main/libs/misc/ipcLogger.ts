b3cf20101b938a4b47c454c7a94b32b3820bce8e
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2025-04-28 11:47:20 +0800

    fix: if webcontents with id 1 not found use first webcontents

--- src/main/libs/misc/ipcLogger.ts
+++ src/main/libs/misc/ipcLogger.ts
@@ -3,7 +3,8 @@ export type LoggerLevel = 'query' | 'error'
 export const ipcLogger = ({ content, cUid, level }: {content: string; cUid: string; level: LoggerLevel}) => {
    if (level === 'error') {
       if (process.type !== undefined) {
-         const mainWindow = require('electron').webContents.fromId(1);
+         const contents = require('electron').webContents.getAllWebContents();
+         const mainWindow = require('electron').webContents.fromId(1) ?? contents[0];
          mainWindow.send('non-blocking-exception', { cUid, message: content, date: new Date() });
       }
       if (process.env.NODE_ENV === 'development' && process.type === 'browser') console.log(content);
@@ -12,7 +13,8 @@ export const ipcLogger = ({ content, cUid, level }: {content: string; cUid: stri
       // Remove comments, newlines and multiple spaces
       const escapedSql = content.replace(/(\/\*(.|[\r\n])*?\*\/)|(--(.*|[\r\n]))/gm, '').replace(/\s\s+/g, ' ');
       if (process.type !== undefined) {
-         const mainWindow = require('electron').webContents.fromId(1);
+         const contents = require('electron').webContents.getAllWebContents();
+         const mainWindow = require('electron').webContents.fromId(1) ?? contents[0];
          mainWindow.send('query-log', { cUid, sql: escapedSql, date: new Date() });
       }
       if (process.env.NODE_ENV === 'development' && process.type === 'browser') console.log(escapedSql);
