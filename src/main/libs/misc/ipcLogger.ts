e6983d205eab3d844a5a7d0f7adfb679deb9c5c6
Fabio Di Stasio <fabio286@gmail.com>
2025-04-28 12:03:30 +0200

    Revert "fix : Cannot read properties of undefined (reading 'send') in mac os"

--- src/main/libs/misc/ipcLogger.ts
+++ src/main/libs/misc/ipcLogger.ts
@@ -3,13 +3,7 @@ export type LoggerLevel = 'query' | 'error'
 export const ipcLogger = ({ content, cUid, level }: {content: string; cUid: string; level: LoggerLevel}) => {
    if (level === 'error') {
       if (process.type !== undefined) {
-         const contents = require('electron').webContents.getAllWebContents();
-         let mainWindow = require('electron').webContents.fromId(1);
-         contents.forEach(content => {
-            if (content.send && mainWindow === undefined) {
-               mainWindow = content;
-            }
-         });
+         const mainWindow = require('electron').webContents.fromId(1);
          mainWindow.send('non-blocking-exception', { cUid, message: content, date: new Date() });
       }
       if (process.env.NODE_ENV === 'development' && process.type === 'browser') console.log(content);
@@ -18,13 +12,7 @@ export const ipcLogger = ({ content, cUid, level }: {content: string; cUid: stri
       // Remove comments, newlines and multiple spaces
       const escapedSql = content.replace(/(\/\*(.|[\r\n])*?\*\/)|(--(.*|[\r\n]))/gm, '').replace(/\s\s+/g, ' ');
       if (process.type !== undefined) {
-         const contents = require('electron').webContents.getAllWebContents();
-         let mainWindow = require('electron').webContents.fromId(1);
-         contents.forEach(content => {
-            if (content.send && mainWindow === undefined) {
-               mainWindow = content;
-            }
-         });
+         const mainWindow = require('electron').webContents.fromId(1);
          mainWindow.send('query-log', { cUid, sql: escapedSql, date: new Date() });
       }
       if (process.env.NODE_ENV === 'development' && process.type === 'browser') console.log(escapedSql);
